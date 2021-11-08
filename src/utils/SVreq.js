d6d55952e8eb4f6eb9d71360ac9b1d3f067c0093
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2021-11-07 21:14:28 -0800

    Added option to check the dates of all near coverage
    This is extremely helpful when searching for coverage in a certain timeframe, such as within the past month.

--- src/utils/SVreq.js
+++ src/utils/SVreq.js
@@ -9,7 +9,24 @@ export default function SVreq(loc, settings) {
 				if (settings.rejectNoDescription && !res.location.description && !res.location.shortDescription) return reject();
 				if (settings.getIntersection && res.links.length < 3) return reject();
 			}
-			if (Date.parse(res.imageDate) < Date.parse(settings.fromDate) || Date.parse(res.imageDate) > Date.parse(settings.toDate)) return reject();
+
+			if (settings.checkAllDates) {
+				if (!res.time || !res.time.length) return reject();
+				var fromDate = Date.parse(settings.fromDate);
+				var toDate = Date.parse(settings.toDate);
+				var dateWithin = false;
+				for (var i = 0; i < res.time.length; i++) {
+					var iDate = Date.parse(res.time[i].jm.getFullYear() + "-" + (res.time[i].jm.getMonth() + 1)); // this will parse the Date object from res.time[i] (like Fri Oct 01 2021 00:00:00 GMT-0700 (Pacific Daylight Time)) to a local timestamp, like Date.parse("2021-09") == 1630454400000 for Pacific Daylight Time
+					if (iDate >= fromDate && iDate <= toDate) { // if date ranges from fromDate to toDate, set dateWithin to true and stop the loop
+						dateWithin = true;
+						break;
+					}
+				}
+				if (!dateWithin) return reject();
+			}
+			else {
+				if (Date.parse(res.imageDate) < Date.parse(settings.fromDate) || Date.parse(res.imageDate) > Date.parse(settings.toDate)) return reject();
+			}
 			if (settings.adjustHeading && res.links.length > 0) {
 				loc.heading = parseInt(res.links[0].heading) + randomInRange(-settings.headingDeviation, settings.headingDeviation);
 			}
