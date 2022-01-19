f02e0a67219183e4b625a8dd7b32b203c3e6eb4c
Bilel Jegham <contact.bileljegham@gmail.com>
2022-01-19 20:52:17 +0000

    lint

--- src/components/exportToGeoJSON.vue
+++ src/components/exportToGeoJSON.vue
@@ -1,40 +1,46 @@
 <template>
-	<Button @click="exportToGeoJson()" class="bordered-success" text="GeoJSON" title="Export as GeoJSON" />
+  <Button
+    @click="exportToGeoJson()"
+    class="bordered-success"
+    text="GeoJSON"
+    title="Export as GeoJSON"
+  />
 </template>
 
 <script setup>
 import Button from "./Elements/Button.vue";
 
 const props = defineProps({
-	selection: Array,
+  selection: Array,
 });
 
 const exportToGeoJson = () => {
-	const features =  props.selection.map(
-		(country) => country.found.map(
-			({lat, lng}) =>({
-	      "type": "Feature",
-      	"geometry": {
-        	"type": "Point",
-        	"coordinates": [
-          	lng,
-						lat
-        	]
-				}      
-	 		})
-	 	)
-	 ).flat();
+  const features = props.selection
+    .map((country) =>
+      country.found.map(({ lat, lng }) => ({
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [lng, lat],
+        },
+      }))
+    )
+    .flat();
 
-	const dataUri = "data:application/vnd.geo+json;charset=utf-8," + encodeURIComponent(JSON.stringify(
-		{
-  		type: "FeatureCollection",
-  		features: features 
-		}
-	));
-	const fileName = `Generated map (${features.length} location${features.length > 1 ? "s" : ""}).geojson`;
-	const linkElement = document.createElement("a");
-	linkElement.href = dataUri;
-	linkElement.download = fileName;
-	linkElement.click();
+  const dataUri =
+    "data:application/vnd.geo+json;charset=utf-8," +
+    encodeURIComponent(
+      JSON.stringify({
+        type: "FeatureCollection",
+        features: features,
+      })
+    );
+  const fileName = `Generated map (${features.length} location${
+    features.length > 1 ? "s" : ""
+  }).geojson`;
+  const linkElement = document.createElement("a");
+  linkElement.href = dataUri;
+  linkElement.download = fileName;
+  linkElement.click();
 };
 </script>
