27fb5853c3242c2a063c1eb4d76bc6a6e84b0c8a
poppos <46404544+poppos@users.noreply.github.com>
2022-05-20 17:45:29 +0200

    Update SVreq.js

--- src/utils/SVreq.js
+++ src/utils/SVreq.js
@@ -3,6 +3,7 @@ const SV = new google.maps.StreetViewService();
 export default function SVreq(loc, settings) {
 	return new Promise(async (resolve, reject) => {
 		await SV.getPanoramaByLocation(new google.maps.LatLng(loc.lat, loc.lng), settings.radius, (res, status) => {
+			(async() => {
 			if (status != google.maps.StreetViewStatus.OK) return reject();
 			if (settings.rejectUnofficial) {
 				if (!/^\xA9 (?:\d+ )?Google$/.test(res.copyright)) return reject();
@@ -14,7 +15,6 @@ export default function SVreq(loc, settings) {
 			const fromDate = Date.parse(settings.fromDate);
 			const toDate = Date.parse(settings.toDate);
 			
-			(async() => {
 			if (settings.checkAllDates) {
 				if (!res.time?.length) return reject();
 
@@ -64,7 +64,6 @@ export default function SVreq(loc, settings) {
 			} else {
 				if (Date.parse(res.imageDate) < fromDate || Date.parse(res.imageDate) > toDate) return reject();
 			}
-			})();
 			loc.lat = res.location.latLng.lat();
 			loc.lng = res.location.latLng.lng();
 
@@ -75,6 +74,8 @@ export default function SVreq(loc, settings) {
 			if (settings.adjustPitch) loc.pitch = settings.pitchDeviation;
 
 			resolve(loc);
+			})();
+
 		}).catch((e) => reject(e.message));
 	});
 }
