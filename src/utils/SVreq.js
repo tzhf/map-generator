eb1f775189c4128aaa4a6b0368b4704b266d9d0c
poppos <46404544+poppos@users.noreply.github.com>
2022-06-14 23:44:22 +0200

    Update SVreq.js

--- src/utils/SVreq.js
+++ src/utils/SVreq.js
@@ -9,7 +9,7 @@ export default function SVreq(loc, settings) {
 				if (!/^\xA9 (?:\d+ )?Google$/.test(res.copyright)) return reject();
 				if (settings.rejectNoDescription && !res.location.description && !res.location.shortDescription) return reject();
 				if (settings.rejectDateless && !res.imageDate) return reject();
-				if (settings.getIntersection && settings.pinpointSearch && res.links.length < 2) return reject();
+				if (settings.pinpointSearch && res.links.length < 2) return reject();
 				if (settings.getIntersection && !settings.pinpointSearch && res.links.length < 3) return reject();
 				console.log(settings.pinpointAngle);
 				if (settings.pinpointSearch && (res.links.length == 2 && Math.abs(res.links[0].heading - res.links[1].heading) > settings.pinpointAngle)) return reject();
