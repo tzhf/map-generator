d0dfda1e39d624d19d0c08f18ec602692967a5e1
poppos <46404544+poppos@users.noreply.github.com>
2022-06-29 17:41:10 +0200

    Update SVreq.js

--- src/utils/SVreq.js
+++ src/utils/SVreq.js
@@ -5,7 +5,7 @@ export default function SVreq(loc, settings) {
 		await SV.getPanoramaByLocation(new google.maps.LatLng(loc.lat, loc.lng), settings.radius, (res, status) => {
 			(async() => {
 			if (status != google.maps.StreetViewStatus.OK) return reject();
-			if (settings.rejectUnofficial) {
+			if (settings.rejectUnofficial && !settings.rejectOfficial) {
 				if (!/^\xA9 (?:\d+ )?Google$/.test(res.copyright)) return reject();
 				if (settings.rejectNoDescription && !settings.rejectDescription && !res.location.description && !res.location.shortDescription) return reject();
 				if (settings.rejectDescription && (res.location.description || res.location.shortDescription)) return reject();
@@ -14,6 +14,9 @@ export default function SVreq(loc, settings) {
 				if (settings.getIntersection && !settings.pinpointSearch && res.links.length < 3) return reject();
 				console.log(settings.pinpointAngle);
 				if (settings.pinpointSearch && (res.links.length == 2 && Math.abs(res.links[0].heading - res.links[1].heading) > settings.pinpointAngle)) return reject();
+			}
+			if (settings.rejectOfficial) {
+				if (/^\xA9 (?:\d+ )?Google$/.test(res.copyright)) return reject();
 			}
 
 			const fromDate = Date.parse(settings.fromDate);
