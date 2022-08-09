1ce552b8f7bc4930a0f52592633aa733d8c70f0e
n-s-j <46404544+n-s-j@users.noreply.github.com>
2022-08-09 23:38:55 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -651,7 +651,7 @@ async function getLoc(loc, country) {
 	}
 	
     if (settings.checkAllDates && res.time && !settings.selectMonths && !settings.rejectOfficial) {
-      SV.getPanorama({ pano: loc.pano}, async (pano, status) => { console.log(pano.res.tiles); });
+      SV.getPanorama({ pano: loc.pano}, async (pano, status) => { console.log(pano); });
       if (!res.time.length) return false;
       const fromDate = Date.parse(settings.fromDate);
       const toDate = Date.parse(settings.toDate);
