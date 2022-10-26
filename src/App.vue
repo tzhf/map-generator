8209c8a0e47b8fa330fea08b864106c355629264
n-s-j <46404544+n-s-j@users.noreply.github.com>
2022-10-26 14:00:37 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -535,16 +535,14 @@ function collapsible_content()
 	var i;
 
 	for (i = 0; i < coll.length; i++) {
-	  coll[i].addEventListener("load", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
+		coll.classList.toggle("active");
+		var content = coll.nextElementSibling;
 		if (content.style.display === "block") {
 		  content.style.display = "none";
 		} 
 		else {
-      		  content.style.display = "block";
-    		}
-	  });
+		  content.style.display = "block";
+		}
 	}
 }
 
